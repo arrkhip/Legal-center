69e3f2118043814f20f2cb525e2adb81b2d5746f
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-01-11 08:05:39 +0300

    created contacts

--- scripts/main.js
+++ scripts/main.js
@@ -33,3 +33,14 @@ $('.js-service__slider').slick({
 		}
 	]
 });
+
+
+// recall slider
+$('.js-recall__slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  dotsClass: 'contacts__recall-slider-dots'
+});
