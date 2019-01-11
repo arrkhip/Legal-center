8d4b6115612b687a50744bf3b68df83d8415b66c
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-01-11 05:51:36 +0300

    finish card-service and service

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,29 @@ $('.js-promo__slider').slick({
   arrows: false,
   dotsClass: 'promo__slider-dots'
 });
+
+
+// service slider
+$('.js-service__slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+	prevArrow: '<button class="service__slider-arrow service__slider-arrow--prev"></button>',
+	nextArrow: '<button class="service__slider-arrow service__slider-arrow--next"></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+		breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	]
+});
